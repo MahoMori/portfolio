a516df7aec0658ef87a649eb50d4ac9b70c3b336
MahoMori <tr27e1104@gmail.com>
2022-05-27 16:16:56 -0700

    Correct spelling mistakes in projectData

--- src/components/projects/projectsData.js
+++ src/components/projects/projectsData.js
@@ -20,7 +20,7 @@ const projectsData = [
     pageLink: "https://product-generator.netlify.app/",
     githubLink: "https://github.com/MahoMori/product-generator",
     description:
-      "This application generates a random product ad and product name by using OpenAI API. The user can generate ads and names multiplle times and choose one each. They can share the generated product on social media and messaging app.",
+      "This application generates a random product ad and product name by using OpenAI API. The user can generate ads and names  multiple times and choose one each. They can share the generated product on social media and messaging apps.",
   },
   {
     title: "Tongue Twister Game",
@@ -46,7 +46,7 @@ const projectsData = [
     figmaLink:
       "https://www.figma.com/file/gXOsP3r6LcIChVVKqX95iN/Vancouver-Hot-Chocolate-Adventure-Design-Draft?node-id=0%3A1",
     description:
-      'Using Geoplocation API, this web application compares user\'s location and a shop location. If they are close enough, the user can check the store as "Been To". The shops and hot chocolate flavours are based on Greater Vancouver Hot Chocolate Festival. All shop data is stored in Firebase.',
+      'Using the Geolocation API, this web application compares a user\'s location and a shop location. If they are close enough, the user can check the store as "Been To". The shops and hot chocolate flavours are based on Greater Vancouver Hot Chocolate Festival. All shop data is stored in Firebase.',
   },
 
   {
@@ -88,7 +88,7 @@ const projectsData = [
     pageLink: "https://togo-todo-list.netlify.app",
     githubLink: "https://github.com/MahoMori/to-go-list.git",
     description:
-      "The website that users can add, edit and delete lists of places to go and things to do. It has complete CRUD feature. It will be implemented with Firebase for signing up and logging in feature so that the list can be for a group of specific people. When users log in, they will have their own panels for an individual list for each person.",
+      "The website that users can add, edit and delete lists of places to go and things to do. It has a complete CRUD feature. It will be implemented with Firebase for signing up and logging in features so that the list can be for a group of specific people. When users log in, they will have their own panels for an individual list for each person.",
     // description:
     //   "The website that users can add, edit and delete lists of places to go and things to do. It has complete CRUD feature. It uses Firebase for signing up and logging in feature so that the list can be for a group of specific people. When users log in, they have their own panels for an individual list for each person.",
   },
@@ -103,7 +103,7 @@ const projectsData = [
     pageLink: "https://cockailedge.netlify.app",
     githubLink: "https://github.com/MahoMori/search-cocktail.git",
     description:
-      "The website to search cocktails by name and ingredients using TheCocktailDB API. It also has a list page of cocktails sorted by alphaneumeric order. The modal pops up when each image is clicked and contains information about the cocktail.",
+      "The website to search cocktails by name and ingredients using TheCocktailDB API. It also has a list page of cocktails sorted by alphanumeric order. The modal pops up when each image is clicked and contains information about the cocktail.",
   },
 
   {
@@ -114,7 +114,7 @@ const projectsData = [
     pageLink: "https://sound-mart-v2.netlify.app",
     githubLink: "https://github.com/MahoMori/sound_mart.git",
     description:
-      "The store page for purchasing BGM. It has trial listening feature and adding items to a cart and deleting items from a cart feature. The number and price in the cart changes depending on items in there. It was a pair poject to practice Javascript with HTML and CSS.",
+      "The store page for purchasing BGM. It has a trial listening feature and adding items to a cart and deleting items from a cart feature. The number and price in the cart changes depending on the items in there. It was a pair project to practice Javascript with HTML and CSS.",
   },
 
   {
@@ -125,7 +125,7 @@ const projectsData = [
     pageLink: "https://udi-lab.netlify.app",
     githubLink: "https://github.com/MahoMori/udi_labo",
     description:
-      "The landing page design for Unnatural Death Investigation Institution. It is a fan made website based on a Japanese drama. The bottom right icon brings users to the top of the website and there is a hamburger menu when viewing in smaller screen than a laptop.",
+      "The landing page design for Unnatural Death Investigation Institution. It is a fan made website based on a Japanese drama. The bottom right icon brings users to the top of the website and there is a hamburger menu when viewing on a smaller screen than a laptop.",
   },
 
   {
@@ -136,7 +136,7 @@ const projectsData = [
     pageLink: "https://light-night-lamp.netlify.app",
     githubLink: "https://github.com/MahoMori/html_css_midterm_project",
     description:
-      "The e-commerce lading page design for the store selling lamps. It showcases some of their products and introduces a service for companies and business to help with lightings at thier offices, cafes and restaurants.",
+      "The ecommerce landing page design for the store selling lamps. It showcases some of their products and introduces a service for companies and businesses to help with lightings at their offices, cafes and restaurants.",
   },
 ];
 
