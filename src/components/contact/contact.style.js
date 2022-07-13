5b7dfa23fadda9f270cc5d0ab0ea27e0cab051ce
MahoMori <tr27e1104@gmail.com>
2022-07-12 21:13:26 -0700

    Comment out font

--- src/components/contact/contact.style.js
+++ src/components/contact/contact.style.js
@@ -66,14 +66,14 @@ export const LeftPanel = styled.div`
 
 export const RightPanel = styled.div`
   text-align: center;
+  ${sansSerif.import}
+  ${sansSerif.fontFamily}
 
   & input,
   & textarea {
     border: solid ${color.skyBlue} 3px;
     font-size: 1.2rem;
     width: 90%;
-    ${sansSerif.import}
-    ${sansSerif.fontFamily}
 
     @media ${device.tablet} {
       width: 85%;
@@ -93,8 +93,8 @@ export const RightPanel = styled.div`
   & textarea {
     padding: 0.25rem;
     resize: vertical;
-    ${sansSerif.import}
-    ${sansSerif.fontFamily}
+    /* ${sansSerif.import}
+    ${sansSerif.fontFamily} */
   }
 `;
 
